ca93da136a74b2b5156a07f181463d1496e0e4c9
Germain LEFEBVRE <germain.lefebvre4@gmail.com>
2021-07-16 22:42:10 +0200

    feat(api): Update docker image from

--- app/core/config.py
+++ app/core/config.py
@@ -12,7 +12,8 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     SERVER_NAME: str = "localhost"
     SERVER_HOST: AnyHttpUrl = "http://localhost:8080"
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ["http://localhost", "http://localhost:8080", "http://localhost:8081", "http://localhost:4200", "http://localhost:3000"]
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ["http://localhost", "http://localhost:8080", "http://localhost:8081", "http://localhost:4200", "http://localhost:3000", \
+        "https://app.cryptobot.germainlefebvre.fr", "https://api.cryptobot.germainlefebvre.fr"]
     # BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
